9057b4e12f0612e58335728f0cf070d8e4a74a2f
Viacheslav Tkachenko <t.viacheslav00@gmail.com>
2025-04-28 17:40:39 +0200

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OrganizationModule } from './features/organizations/organization.module';
 import { LoggerMiddleware } from './core/middlewares/logger.middleware';
 import { EnvModule } from './core/env/env.module';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './core/env/env';
-import { HealthModule } from './features/health/health.module';
 import { UsersModule } from './features/users/users.module';
 import { FlagsModule } from './features/flags/flags.module';
 
@@ -16,8 +14,6 @@ import { FlagsModule } from './features/flags/flags.module';
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
-    HealthModule,
-    OrganizationModule,
     EnvModule,
     UsersModule,
     FlagsModule,
