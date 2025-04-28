5bdc08432aefcf93335ca3dfc739d5242fb74bf1
Viacheslav Tkachenko <t.viacheslav00@gmail.com>
2025-04-28 21:17:03 +0200

    init

--- src/core/common-module/common-module.module.ts
+++ src/core/common-module/common-module.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { EnvModule } from './env/env.module';
-import { envSchema } from './env/env';
 import { DatabaseModule } from './db/database.module';
 import { DiscordModule } from './discord/discord.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
-      isGlobal: true,
-    }),
-    EnvModule,
-    DatabaseModule,
-    DiscordModule,
-  ],
+  imports: [EnvModule, DatabaseModule, DiscordModule],
   providers: [],
   exports: [EnvModule, DatabaseModule, DiscordModule],
 })
--- src/core/common-module/env/env.module.ts
+++ src/core/common-module/env/env.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EnvService } from './env.service';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   providers: [EnvService],
   exports: [EnvService],
 })
